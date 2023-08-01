ab3578fc8979fc5b9732d3d411bab6169a1323ba
Manny Espinoza <84434314+e-Manny@users.noreply.github.com>
2023-07-31 22:37:04 -0700

    Created sign in and sign up pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,41 @@
+import Navbar from "../components/Navbar";
+export default function SignIn() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <div
+        className="d-flex align-items-center py-4 bg-body-tertiary"
+        style={{ height: "100vh" }}
+      >
+        <main className="form-signin m-auto" style={{ width: "300px" }}>
+          <form>
+            <h1 className="h3 mb-3 fw-normal">Sign up!</h1>
+
+            <div className="form-floating">
+              <input
+                type="email"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+              ></input>
+              <label htmlFor="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                type="password"
+                className="form-control"
+                id="floatingPassword"
+                placeholder="Password"
+              ></input>
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <button className="btn btn-primary w-100 py-2 my-3" type="submit">
+              Sign in
+            </button>
+            <p className="mt-5 mb-3 text-body-secondary">© 2017–2023</p>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+}

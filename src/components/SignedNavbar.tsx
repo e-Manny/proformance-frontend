fe2344dcbc41873e927760e1ed09e7641f6f5078
Manny Espinoza <84434314+e-Manny@users.noreply.github.com>
2023-08-16 12:20:48 -0700

    confirgured logout on signed navigation bar

--- src/components/SignedNavbar.tsx
+++ src/components/SignedNavbar.tsx
@@ -1,5 +1,16 @@
 import { Link } from "react-router-dom";
+import httpClient from "../httpClient";
+import { useNavigate } from "react-router-dom";
 export default function SignedNavbar() {
+  const navigate = useNavigate();
+  const userSignout = async () => {
+    try {
+      await httpClient.post("http://127.0.0.1:5000/logout");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <nav className="py-2 bg-body-tertiary border-bottom ">
       <div className="container d-flex flex-wrap">
@@ -26,7 +37,11 @@ export default function SignedNavbar() {
         </ul>
         <ul className="nav">
           <li className="nav-item">
-            <Link to="#" className="nav-link link-body-emphasis px-2">
+            <Link
+              to="/"
+              className="nav-link link-body-emphasis px-2"
+              onClick={userSignout}
+            >
               Sign Out
             </Link>
           </li>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,29 +1,44 @@
+import { useState, useEffect } from "react";
 import Features from "../components/Features";
 import Footer from "../components/Footer";
 import HomeHero from "../components/HomeHero";
 import Navbar from "../components/Navbar";
-// import SignedFeatures from "../components/SignedFeatures";
-// import SignedHomeHero from "../components/SignedHomeHero";
-// import SignedNavbar from "../components/SignedNavbar";
+import SignedFeatures from "../components/SignedFeatures";
+import SignedHomeHero from "../components/SignedHomeHero";
+import SignedNavbar from "../components/SignedNavbar";
+import httpClient from "../httpClient";
 
 export default function Landing() {
-  // let isSignedIn: boolean = false;
+  const [userID, setUserID] = useState("");
 
-  // if (isSignedIn == true) {
-  //   return (
-  //     <>
-  //       <SignedNavbar></SignedNavbar>
-  //       <SignedHomeHero></SignedHomeHero>
-  //       <SignedFeatures></SignedFeatures>
-  //       <Footer></Footer>
-  //     </>
-  //   );
-  // } else {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await httpClient.get("http://127.0.0.1:5000/@me");
+        setUserID(resp.data.id);
+        console.log(resp.data);
+      } catch (error) {
+        console.log("Not authenticated");
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Navbar></Navbar>
-      <HomeHero></HomeHero>
-      <Features></Features>
+      {userID != "" ? (
+        <>
+          <SignedNavbar></SignedNavbar>
+          <SignedHomeHero></SignedHomeHero>
+          <SignedFeatures></SignedFeatures>
+        </>
+      ) : (
+        <>
+          <Navbar></Navbar>
+          <HomeHero></HomeHero>
+          <Features></Features>
+        </>
+      )}
       <Footer></Footer>
     </>
   );
